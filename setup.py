337210cbbc598fe7f73e919425c042b2998bf0bc
Bryan Herman <hermab53@gmail.com>
2015-12-22 21:34:54 -0500

    built loss matrix

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Diffusion',
+      version='1.0',
+      description='1-D Diffusion Code',
+      author='Bryan Herman',
+      author_email='hermab53@gmail.com',
+      packages=['diffusion'],
+     )
